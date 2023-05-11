664641a11f7318d176191b4da32c312c81f23ad5
eunseo1028 <105277385+eunseo1028@users.noreply.github.com>
2023-05-11 23:45:02 +0900

    Add files via upload

---
+++ PlayrtController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayrtController : MonoBehaviour
+{
+    public Rigidbody playerRigidbody; // 이동에 사용할 리지드바디 컴포넌트
+    public float speed = 8f; // 이동 속력
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(Keycode.UpArrow) == true){
+            // 위쪽 방향키 입력이 감지된 경우 z 방향 힘 주기
+            playerRigidbody.AddForce(0f, 0f, speed);
+
+        }
+        if (Input.GetKey(Keycode.DownArrow) == true)
+        {
+            // 아래쪽 방향키 입력이 감지된 경우 -z 방향 힘 주기
+            playerRigidbody.AddForce(0f, 0f, -speed);
+
+        }
+        if (Input.GetKey(Keycode.RightArrow) == true)
+        {
+            // 오론쪽 방향키 입력이 감지된 경우 x 방향 힘 주기
+            playerRigidbody.AddForce(speed, 0f, 0f);
+
+        }
+        if (Input.GetKey(Keycode.LeftArrow) == true)
+        {
+            // 왼쪽 방향키 입력이 감지된 경우 -x 방향 힘 주기
+            playerRigidbody.AddForce(-speed, 0f, 0f);
+
+        }
+
+    }
+}
