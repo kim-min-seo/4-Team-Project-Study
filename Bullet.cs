c14e128c28a6bab34396c2e023ebcc7e256e9cdf
eunseo1028 <105277385+eunseo1028@users.noreply.github.com>
2023-05-25 00:04:59 +0900

    = = 사이에 공백 수정

--- Bullet.cs
+++ Bullet.cs
@@ -18,7 +18,7 @@ public class Bullet : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         // 충돌한 상대방 게임 오브젝트가 미ayer 태그를 가진 경우
-        if (other.tag = = "Player")
+        if (other.tag == "Player")
         {
             // 상대방 게임 오브젝트에서 PlayerController 컴포넌트 가져오기
             PlayerController playerController = other.GetComponent<PlayerController>();
