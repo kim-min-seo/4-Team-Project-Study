373bd1ce3cf7c2ed8efaee9b266dea8433390fab
eunseo1028 <105277385+eunseo1028@users.noreply.github.com>
2023-06-01 22:32:18 +0900

    Update Bullet.cs

--- Bullet.cs
+++ Bullet.cs
@@ -17,7 +17,7 @@ public class Bullet : MonoBehaviour
     // 트리거 충돌 시 자동으로 실행되는 메서드
     void OnTriggerEnter(Collider other)
     {
-        // 충돌한 상대방 게임 오브젝트가 미ayer 태그를 가진 경우
+        // 충돌한 상대방 게임 오브젝트가 Player 태그를 가진 경우
         if (other.tag == "Player")
         {
             // 상대방 게임 오브젝트에서 PlayerController 컴포넌트 가져오기
