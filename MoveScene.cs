bf973a3bc501fdd72c7629904695cb07f7073f9b
eunseo1028 <105277385+eunseo1028@users.noreply.github.com>
2023-06-01 22:31:10 +0900

    Update MoveScene.cs

--- MoveScene.cs
+++ MoveScene.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     
     // Update is called once per frame
     void Update()
