2bdc586917fefc032be6d5e1234dc180afa64269
eunseo1028 <105277385+eunseo1028@users.noreply.github.com>
2023-06-01 22:59:34 +0900

    Update Rotator.cs

--- Rotator.cs
+++ Rotator.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
     public float rotationSpeed = 60f;
+    private float timer = 5f; // 5초 후 회전
+    private bool rotationStarted = false;
 
     void Updat()
     {
